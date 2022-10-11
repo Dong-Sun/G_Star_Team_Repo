baf54ac34f14df07880a98cb6732b88cba50e960
dong-sun <duaehdtjs20@gmail.com>
2022-10-11 12:45:47 +0900

    #5 Switch 상호작용, #9 Spike 구현
    + PlayerInteration 에서 Obstacle이 쓸모 없는 걸로 판단되어 삭제했습니다. 그 밖에 Bullet에서 충돌 처리 시 플레이어가 맞는 기능이 아직 미구현이기 때문에 추후에 로직 수정이 필요합니다.

---
+++ Assets/Scripts/Obstacle/Bullet.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Spike에서 나오는 총알의 기능을 구현한 클래스입니다.
+/// </summary>
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;  // 총알의 이동속도
+    float destroyTimer = 5f;            // 총알이 자체적으로 사라지는데 걸리는 시간
+    float timer = 0f;
+    Spike spike;                        // 재생성을 위한 부모클래스 할당
+    private void Start() {
+        spike = transform.parent.GetComponent<Spike>();
+    }
+    void Update()
+    {
+        transform.Translate(-transform.forward * Time.deltaTime * speed);   // 전방으로 이동
+        timer += Time.deltaTime;
+        if(timer > destroyTimer) {
+            Init();
+        }
+    }
+    private void Init() {   // 총알 재활용을 위해서 데이터들을 초기화 시켜주는 함수
+        transform.localPosition = Vector3.zero;
+        spike.setBullet = true;
+        timer = 0f;
+        gameObject.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other) {   // 총알 충돌 처리
+        if (other.transform.tag == "Player") {
+            Debug.Log("Hit Player");
+        }
+        Init();
+    }
+}

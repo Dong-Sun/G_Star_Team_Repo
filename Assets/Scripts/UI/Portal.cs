774311c7135f3b1acef16875bb6ce4274a9d963f
LeeYuJun98 <pok_gean@naver.com>
2022-10-25 01:49:31 +0900

    문 작업

--- Assets/Scripts/UI/Portal.cs
+++ Assets/Scripts/UI/Portal.cs
@@ -5,11 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name=="Player")
+        if (other.gameObject.tag == "Player")
         {
+            GameManager.Game_Manager_Instance.Game_Stop = true;
+            GameManager.Game_Manager_Instance.Rapping_invoke(3);
             SceneLoadManager.scene_load_manager_instance.NextSceneLoad();
         }
     }
+
 }

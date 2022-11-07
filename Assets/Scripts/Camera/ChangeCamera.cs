f311ecc8e9f639420aed455686e9a5634166f71f
dong-sun <duaehdtjs20@gmail.com>
2022-11-07 11:11:10 +0900

    5층

--- Assets/Scripts/Camera/ChangeCamera.cs
+++ Assets/Scripts/Camera/ChangeCamera.cs
@@ -1,17 +1,16 @@
 using UnityEngine;
+using DataStruct;
 
 public class ChangeCamera : MonoBehaviour {
     [SerializeField] GameObject main;   // 플레이 중인 카메라
     [SerializeField] GameObject start;  // 시작할 때 나오는 문을 보여주는 카메라
-    [SerializeField] GameObject end;    // 끝날 때 문에 들어가면서 들어가는 장면을 보여주는 카메라
+    [SerializeField] GameObject[] end;    // 끝날 때 문에 들어가면서 들어가는 장면을 보여주는 카메라
 
     private void Start() {
         ChangeToStart();    // 처음 시작할 대는 start 카메라를 켜주고
 
     }
-
     private void Update() {
-        // 테스트용 숫자키로 카메라 전환
         if (Input.GetKeyDown(KeyCode.Alpha1))
             ChangeToStart();
         if (Input.GetKeyDown(KeyCode.Alpha2))
@@ -23,18 +22,42 @@ public class ChangeCamera : MonoBehaviour {
     public void ChangeToMain() {
         main.SetActive(true);
         start.SetActive(false);
-        end.SetActive(false);
+        EndCameraOff();
     }
 
     public void ChangeToStart() {
         main.SetActive(false);
         start.SetActive(true);
-        end.SetActive(false);
+        EndCameraOff();
     }
 
     public void ChangeToEnd() {
         main.SetActive(false);
         start.SetActive(false);
-        end.SetActive(true);
+        EndCameraOn();
+    }
+
+    private void EndCameraOff() {
+        foreach (GameObject g in end)
+            g.SetActive(false);
+    }
+    private void EndCameraOn() {
+        switch (GameManager.Game_Manager_Instance.Game_Dir) {
+            case Dir.ForWard:
+                end[0].SetActive(true);
+                break;
+            case Dir.Left:
+                end[1].SetActive(true);
+                break;
+            case Dir.BackWard:
+                end[2].SetActive(true);
+                break;
+            case Dir.Right:
+                end[3].SetActive(true);
+                break;
+            default:
+                break;
+        }
+        
     }
 }
